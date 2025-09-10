33e58f5dfeb35ea23579516267e2756c41bdb84e
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-09-10 08:25:00 -0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,111 +1,128 @@
-
-"""
+""" 
 file name: variables.py
 
-Live coding: I write code in one pytheon file and have my notes in multiline comments in between code
+Live coding: I write code in one python file and have my notes in multiline comments in between code 
 segments
 
-what is python?
+What is python? 
 
-    Python is a programing language
-        a set of syntax and that you write instructions in for your computer to follow
+    - Python is a programming language
+        - A set of syntax and protocols that you write instructions in for your computer to follow 
+    
+    - Python is a command line program
+        - When you download python off the internet, you're downloading what is actually called
+        the python interpreter
         
-        python is a command line prograsm
-        when you download python odd the internet you're downloading what is actually called the python interepreter
-        the interpretors role on your computer
-        it looks at .py files and line by line convert valid python syntax into machine ConnectionAbortedError
-    python is an interepreted language
-    we just point the intereprerter aat'a python file and itl'll run the ConnectionAbortedError
-    compiled languages
-    c, c++, java, etc.
-    call the compiler on your code file and till hwt cxompiled into an exicutable file tyhat you Run
-    trhe first thing that is of importance tro us are variables
-    the name of the variable
-    its data type
-    its Attribute <= the actual variabvle itself
-    """
+    - The interpreter's role on your computer
+        - It looks at .py files, and line by line convert valid python syntax into machine code
+        
+    - Python is an interpreted language
+        - We just point the interpreter at a python file and it'll run the code
+        
+    - Compiled languages
+        - c, c++, java, etc. 
+        - Call the compiler on your code file, and it'll get compiled into an executible file that you
+        run 
+        
+    The first thing that is of importance to us are variables. 
     
-x=5
-#^ This is a variable
-#<name of your variable = <attribute>
+    Variables carry three properties:
+        - The name of the variable
+        - Its data type
+        - Its attribute <= the actual data itself
+        
+    A variable is just a named place on your computer's RAM that stores information
     
+    
+"""
+x = 5
+#^ This is a variable 
+#<name of your variable> = <attribute> 
 print(x)
 
-"""
-    Data types
-    numbers
-    strings of text
+""" 
+Data Types: 
+
+    - Numbers
+    - Strings of text 
     
-    there are two kinda of data types
-        -integer a whole number, including 0 and negative numbers
-        - float floating point number, pi
-        
-    """
+There are two datatypes for numbers
+    - Integer: A whole number, including 0 and negative numbers
+    - Float: Floating Point Number, pi 
+    
+"""
 x = 5
-    #this is a variable named x of type integer, of value 5
-    
+#This is a variable named x, of type integer, of value 5 
+
+""" 
+We can add, subtract, multiply, divide, and square numbers
 """
-    we can add subtract multiply divide and sqyare numbers
-    """
 y = 10
-    
-    #Addition
+#Addition
 print(x + y)
-    #Subtraction
+#Subtraction
 print(x - y)
-    #Multiplication
+#Multiplication
 print(x * y)
-    #Division
+#Division
 print(x / y)
-    #Squaring a number
-print(x ** 2)
-    
+#Squaring a number
+print(x**2)
+
+""" 
+Sometimes when we're manipulating numbers, we want to work with the results again later on. 
+
+All we did was print the results of our arithmetic. 
+
+The python interpreter is really not that smart, it doesn't remember what it prints. So in order for 
+python to remember what it did, you need to save it back into another variable (or itself)
 """
-    sometuimes when we're manipulating numbers we want to qworek with the results again later on
-    
-    all we did was print thge results of our arithmatic
-    
-    the pythoin interpreter is reallu not thst smart it doesnty remembere whayt it prints so in order fpr
-    python to remember what it did you nweeed to save it back into another variable or itself
-    """
-sum = x+y
+sum = x + y
 print(sum)
+""" 
+So let's keep sum in mind and also add another variable to it. 
+
+The equals sign is really just an assignment operator and not an equality operator.
+
+common things that happen:
+    - you'll have a variable that you need to update repeatedly
 """
-    so lets keep sun in mind and also add another variable tyo it
-    
-    thwe equals sign is reeaLLYU just an assignment operator and not an equality operator
-    
-    common things trhat happen
-    you'll have a variabv;e that youy nwws to update repeatedly
-    """
-    
-sum - sum + 20 #This is perfectly valid
-"""sum is a named place in memory
-    
-    on the left hand side od the equals sign we're declatinmg a variable
-    on the right hand sidfe we substitute the value of sum into where called it
-    
-    sum = x+y
-    =15
-    
-    sum=sum+20
-    sum=35
-    """
+
+sum = sum + 20 #This is perfectly valid 
+""" 
+Sum is a named place in memory
+
+on the left hand side of the equals sign, we're declaring a variable
+on the right hand side, we substitute the value of sum into where called it
+
+sum = x + y 
+    = 15
+
+sum = sum + 20
+      15 + 20
+      
+sum = 35 
+"""
 print(sum)
+
+""" 
+Strings of text: 
+
+    - In other programming langauges, you have a data type specifically for single characters of text
+    - In python, strings of text are all the same data type (string)
 """
-    sritngs of text in other programming langauages uyou have a data type specifically for singlwe chaeracters of text
-    in poython strings of text are all the samwe data tyle (string)
-    """
+
 name = "Jackson Sommerfeld"
-    #<namer of the variable> = <attributre>
+#<name of the variable> = <attribute>
 print(name)
-    
-    #what if i want to print out "my name is"
+
+#What if I want to print out "My name is <name>"
+
+""" 
+In python, we can do this in two ways. 
+
+    - fstrings: format string and it lets specify placeholder values in strings so you can drop in variables
     
 """
-    in python we can do this in two ways
-    
-    fstrings format steingf and it lets specity placeholder values in strings so you can dop im variables
-    
-    """
 print(f"My name is {name}")
+print(f"the sum of x and y is {sum}")
