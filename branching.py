1c9778a28e791fb78850604096528b4bdd7a3d33
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-09-15 11:32:03 -0400

    Create branching.py

---
+++ branching.py
@@ -0,0 +1,18 @@
+"""
+boolean
+conditional = logical statment that can be evaluated to a boolean value
+if conditional>;
+    <MUST BE TABBED OVER code block>
+<when you're done, untab>
+
+modulus: %
+
+a % b = a mod b = remainder after a/b
+
+modulus to check divisibility rules
+ONE EQUALS (=) for variable assignment TWO EQUALS (==) for equality checking
+"""
+
+x = 10
+if x % 2 == 0:
+    print(f"{x} is divisible by 2")
