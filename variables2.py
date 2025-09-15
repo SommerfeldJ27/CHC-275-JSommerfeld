dfb3e6b489006531ecdbe2970ed2aea556195622
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-09-15 15:09:30 -0400

    Update variables2.py

--- variables2.py
+++ variables2.py
@@ -1,47 +1,73 @@
-"""
-    we want keyboard input
-    hardcoding values into programs is not exactly how we use computers
-    we want some sort of input and output from the computer based on what the user does
-    it prints the prompt into the terminal
-    it scans the keyboard for input
-    saves the input into a variable
-    input what is the first number
-    out objecxtibe is take two numbers and add trhem together
-"""
-numsix = input("enter your number ")
-lorkeez = input("enter your second number ")
-sum = numsix + lorkeez
-print(f"The sum is {sum}")
+""" 
+variables2.py
 
+We want keyboard input.
+
+Hardcoding values into programs is not exactly how we use computers
+we want some sort of input and output from the computer based on what the user does. 
+
+For example, if we wanted to have a calculator, we would want the user to be able to 
+choose the numbers to be added before runtime. Thankfully in python, doing this is relatively easy
+
+there is an entire function in python dedicated to keyboard input 
+
+input(<prompt>) <= this function does 3 things
+
+    1) It prints the prompt into the terminal
+    2) It scans the keyboard for input
+    3) Saves the input into a variable
+    
+input("what is the first number")
+
+Our objective is: take two numbers and add them together
 """
-the user is going to type in
+
+""" 
+The user is going type in 
 
 numsix = 10
 lorkeez = 5
 sum = numsix + lorkeez
 
-what do you expecty to be printed. we expect 15
+what do you expect to be printed. we expect 15
 
-we got 105 that is not the right answer the thing is about input is when it scans the keyboard it assigns the variable to the string data type
-the name
-the data type
-the attribute
-numsix
-the name numsix
-string
-"10"
+We got 105, that is not the right answer. The thing is about input, is when it scans the keyboard it
+assigns the variable to the string data type
 
-you cannot do m,ath with stings we do smth called typecasting
-typecasting reassigfns the datatype of the bariabnlew as long as its a valid target
-numsix = "Jackson Sommerfeld" <- can't typecast
-numsix = "10" <- I can typecast this
-to change a string to a number
+Variables have 3 pieces of information
+    1) The name
+    2) The data type 
+    3) The attribute 
+    
+numsix 
+    1) the name numsix
+    2) String
+    3) "10" 
+    
+You cannot do math with strings. We do something called typecasting
+
+Typecasting = Reassigning the datatype of the variable as long as its a valid target 
+
+numsix = "Jackson Sommerfeld" <- i want to typecast this into a number 
+numsix = "10" <- I can typecast 
+
+to change a str -> int
+
+
 """
 
-numsix = input("enter your number ")
+
+numsix = input("Enter your number ")
 numsix = int(numsix)
-    #int(variable name)
-lorkeez = input("enter your second number ")
-numsix = int(lorkeez)
+        #int(variable name) <= will typecast your variable into integer (as long as its allowed)
+lorkeez = input("Enter your second number ")
+lorkeez = int(lorkeez)
+        #now numsix and lorkeez are both of datatype integer (They are both numbers!)
 sum = numsix + lorkeez
 print(f"The sum is {sum}")
+
+""" 
+input() ALWAYS gives a string -> You need to typecast this into the correct datatype that you're expecting
+
+
+"""
