50d170d14bb906345003e5a0f8725c2de6de5a4b
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-09-16 14:37:58 -0400

    Create branching2.py

---
+++ branching2.py
@@ -0,0 +1,15 @@
+print("welcome to the lunch loan program")
+print("1 Freshman")
+print("2 Sophomore")
+print("3 Junior")
+print("4 Senior")
+option = input("Enter your grade level: ")
+
+if (option == "Freshman"):
+    print("You get 5 dollars")
+if (option == "Sophomore"):
+    print("You get 7 dollars")
+if (option == "Junior"):
+    print("You get 9 dollars")
+if (option == "Senior"):
+    print("You get 11 dollars")
