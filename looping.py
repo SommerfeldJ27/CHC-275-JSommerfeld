04202dfe115213a5f357ac55edaa60c04de24c37
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-09-17 13:47:50 -0400

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,45 @@
+"""
+    - Sequential
+    - Branching
+    - Repetition
+
+We are interested in repeating code blocks over and over again
+"""
+
+print(1)
+print(1)
+print(1)
+print(1)
+print(1)
+print(1)
+print(1)
+print(1)
+print(1)
+print(1)
+print(1)
+
+"""
+This already feels wrong and innefficient abd isn't really generalizable
+
+There are 3 ways to repeat a code segment
+    - While loops
+    - For loops
+    - Recursion
+While loops are the easiest
+"""
+
+
+    
+"""
+Python Interpreter allocates partitions of RAM to two different collections
+    - Stack: fixed ammount of memory for function calls and variable assignment
+    - Heap: dynamic ammount of memory for complex gaming structures
+"""
+    
+nums = [1,2,3,4,5,6,7,8,9,10]
+x = 0
+sum = 0
+while x < len(nums):
+        sum += nums[x]
+        x +=1
+print(f"the sum is {sum}")
