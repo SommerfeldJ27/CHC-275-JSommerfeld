d31f9545e1e2b1f40cff93a8e90f79655c10a789
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-09-24 15:57:51 -0400

    Update fish.py

--- fish.py
+++ fish.py
@@ -1,20 +1,20 @@
         
-print("What kinda fishy do you have? ")
+print("What kind of fish do you have? ")
 print("Carnivourous")
 print("Salt Water")
 print("Community")
 option = input("Enter Fish Type: ")
 
 if (option == "Carnivourous"):
-    print("do you already have it?")
+    print("Do you already have it?")
     option = input("")
-    if (option == "yes"):
-             print("too bad")
-    if (option == "no"):
-            print("enjoy")
-elif (option == "salt water"):
-    print("you're a fancy fish parent")
-elif (option == "community"):
-    print("you should get more than one")
+    if (option == "Yes"):
+             print("Too bad")
+    if (option == "No"):
+            print("Enjoy")
+elif (option == "Salt Water"):
+    print("You're a fancy fish parent")
+elif (option == "Community"):
+    print("You should get more than one")
 else:
-    print("i dont think thats a kind of fish")
+    print("I don't think thats a kind of fish")
