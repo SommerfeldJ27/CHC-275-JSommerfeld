79f65e6960e780ab3d2c03a06a850fc399f198d5
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-09-25 13:58:43 -0400

    Update hailstone.py

--- hailstone.py
+++ hailstone.py
@@ -1,11 +1,11 @@
 x = input("Enter a number: ")
 x = int(x)
 
-while x != 1:
+while x == 1 == False:
     print (x)
     if x % 2 == 0:
         x = x/2
     elif x % 2 == 1:
         x = x * 3 + 1
-if x == 1:
+if x == 1 == True:
     print(x)
