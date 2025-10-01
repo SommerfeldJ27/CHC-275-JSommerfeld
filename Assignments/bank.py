f4c03cff8334fe8a9d42ec8f0533893035bb13fa
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-10-01 11:26:05 -0400

    Update bank.py

--- Assignments/bank.py
+++ Assignments/bank.py
@@ -1,3 +1,5 @@
-"""
-This is our next assignment
-"""
+name = ["James","Jack","John", "Paul"]
+balance = [102,71,93,88]
+
+for i in range(len(name)):
+     print(f"Name: {name[i]} Balance: {balance[i]}") 
