e91392630fa95f0d1361b3f7e3afa6c2ae802e36
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-10-02 14:45:11 -0400

    Update bank.py

--- Assignments/bank.py
+++ Assignments/bank.py
@@ -1,28 +1,40 @@
-print("Welcome to FFCU! How can I help you today? ")
-print("Deposit or Withdraw")
-print("Transfer Money")
-print("List Accounts and Balances")
-print("Add or Remove Accounts")
-option = input("")
+accounts = ["Smasher" , "Silverhand", "Rogue", "Jackie", "V"]
+balances = [1000, 2000, 3000, 4000, 5000]
+check = False
 
-if (option == "Deposit or Withdraw"):
-    print("Will you be depositing or withdrawing funds?")
-    option = input("")
-    if (option == "Depositing"):
-             print("Good idea") #This is a place holder for later
-    if (option == "Withdrawing"):
-            print("Enjoy") #This is a place holder for later
-elif (option == "Tranfer Money"):
-    print("To who?") #This is a place holder for later
-elif (option == "List Accounts and Balances"):
-    print("here they are") #This is a place holder for later
-elif (option == "Add Account"):
-    print("Hello Mr. Account") #This is a place holder for later
-elif (option == "Remove Account"):
-     print("Goodbye Mr. Account") #This is a place holder for later
-     
-name = ["James","Jack","John", "Paul"]
-balance = [102,71,93,88]
-
-for i in range(len(name)):
-     print(f"Name: {name[i]} Balance: {balance[i]}") 
+while check == False:
+    for i in range(len(accounts)):
+        print(accounts[i])
+    
+    print("Welcome to FFCU! How can I help you today? ")
+    print("1. Deposit or Withdraw")
+    print("2. Transfer Money")
+    print("3. List Accounts and Balances")
+    print("4. Add or Remove Accounts")
+    option = input("Select option or type quit to exit: ")
+    if (option == "1"):
+        print("Will you be depositing or withdrawing funds?")
+        option = input("")
+        if (option == "Depositing"):
+                print("Good idea") #This is a place holder for later
+        if (option == "Withdrawing"):
+                print("Enjoy") #This is a place holder for later
+    elif (option == "2"):
+        print("To who?") #This is a place holder f holor later
+    elif (option == "3"):
+        print("here they are") #This is a place holder for later
+    elif (option == "4"):
+        print("Will you be adding or removing an account?")
+        option = input("")
+        if (option == "Adding"):
+                name = input("Enter Account Name:")
+                accounts.append(name)
+                bal = input("Current Funds: ")
+                bal = int(bal)
+        if (option == "Removing"):
+                name = input("Enter Account Name:")
+                index = accounts.append(name)
+                accounts.pop(index)
+                balances.pop(index)
+    elif (option == "Quit"):
+        check = True
