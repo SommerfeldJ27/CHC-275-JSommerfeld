bd39a8f3efce3a9cf2cadb08354fbc17c1897755
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-10-14 13:35:11 -0400

    Create strings2.py

---
+++ Notes/strings2.py
@@ -0,0 +1,14 @@
+mystr = "Frank Teague Preston"
+names = mystr.split()
+print(names)
+
+mystr = "Hello World How Are You"
+msg = mystr.split("11010101")
+print(msg)
+
+space = " "
+newmsg = space.join(msg)
+print(newmsg)
+
+newmsg = " ".join(msg)
+print(newmsg)
