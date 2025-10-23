34cdb5a1640d6df3ef34cc1098d2eb8e694a9ab9
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-10-22 23:03:58 -0400

    Update palindrome.py

--- Assignments/palindrome.py
+++ Assignments/palindrome.py
@@ -1,4 +1,26 @@
-"""
-Write a program that detects if a word is a palindrome or not.
-"""
-word = input("Please Enter Your Word: ").strip().lower()
+def is_palindrome(option):
+
+    palindrome = option
+
+    left, right = 0, len(palindrome) - 1
+
+    while left < right:
+
+        if palindrome[left] == " ":
+            left += 1
+        if palindrome[right] == " ":
+            right -= 1
+
+        if palindrome[left] != palindrome[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+option = input("Enter a word: ")
+if is_palindrome(option):
+    print("It's a palindrome")
+else:
+    print("It's not a palindrome")
