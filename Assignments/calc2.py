e877d8c53a46011088c17c9ae25321d0c025a0b2
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-10-29 13:12:46 -0400

    Update calc2.py

--- Assignments/calc2.py
+++ Assignments/calc2.py
@@ -1,28 +1,42 @@
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+option = input("Please Select Preferred Function or Type 'Quit' To Exit: ")
+
 check = False
 while check == False:
-    option = input("Please Select Preferred Function or Type 'Quit' To Exit: ")
-if option == ("Add").strip().lower():
-    num1 = int(input("Enter Num 1"))
-    num2 = int(input("Enter Num 2"))
-    sum = num1 + num2
-    print(f"the sum of {num1} and {num2} is {sum}")
-elif option == ("Subtract").strip().lower():
-    num3 = int(input("Enter Num 3"))
-    num4 = int(input("Enter Num 4"))
-    difference = num3 - num4
-    print(f"the difference of {num3} and {num4} is {difference}")
-elif option == ("Multiplication").strip().lower():
-    num5 = int(input("Enter Num 5"))
-    num6 = int(input("Enter Num 6"))
-    product = num5 * num6
-    print(f"the product of {num5} and {num6} is {product}")
-elif option == ("Divide").strip().lower():
-    num7 = int(input("Enter Num 7"))
-    num8 = int(input("Enter Num 8"))
-    if (num7 == 0 and num8 != 0) or (num7 and num8 != 0):
-        quotient = num7/num8
-        print(quotient)
-    if num7 != 0 and num8 == 0:
-        print("You Cannot Divide by 0")
-elif option == ("Quit"):
-    check == True
+    if option == ("Add").strip().lower():
+        try:
+            num1 = int(input("Enter Num 1: "))
+            num2 = int(input("Enter Num 2: "))
+            sum = num1 + num2
+            print(f"The sum of {num1} and {num2} is {sum}")
+        except ValueError:
+            print("Not a proper value")
+    elif option == ("Subtract").strip().lower():
+        try:
+            num3 = int(input("Enter Num 3: "))
+            num4 = int(input("Enter Num 4: "))
+            difference = num3 - num4
+            print(f"The difference of {num3} and {num4} is {difference}")
+        except ValueError:
+            print("Not a proper value")
+    elif option == ("Multiplication").strip().lower():
+        try:
+            num5 = int(input("Enter Num 5: "))
+            num6 = int(input("Enter Num 6: "))
+            product = num5 * num6
+            print(f"The product of {num5} and {num6} is {product}")
+        except ValueError:
+            print("Not a proper value")
+    elif option == ("Divide").strip().lower():
+        try:
+            num7 = int(input("Enter Num 7: "))
+            num8 = int(input("Enter Num 8: "))
+            quotient = num7/num8
+            print(f"The quotient of {num7} and {num8} is {quotient}")
+        except ValueError:
+            print("Not a proper value")
+    elif option == ("Quit").strip().lower():
+        check == True         
