f006ce22c8a52863d711038a4f4f23e764b6dd43
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-10-30 11:20:06 -0400

    Update calc2.py

--- Assignments/calc2.py
+++ Assignments/calc2.py
@@ -1,12 +1,11 @@
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-option = input("Please Select Preferred Function or Type 'Quit' To Exit: ")
-
 check = False
 while check == False:
-    if option == ("Add").strip().lower():
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    option = input("Please Select Preferred Function or Type 'Quit' To Exit: ").strip().lower()
+    if option == "1" or option == "add":
         try:
             num1 = int(input("Enter Num 1: "))
             num2 = int(input("Enter Num 2: "))
@@ -14,7 +13,7 @@ while check == False:
             print(f"The sum of {num1} and {num2} is {sum}")
         except ValueError:
             print("Not a proper value")
-    elif option == ("Subtract").strip().lower():
+    elif option == "2" or option == "subtract":
         try:
             num3 = int(input("Enter Num 3: "))
             num4 = int(input("Enter Num 4: "))
@@ -22,7 +21,7 @@ while check == False:
             print(f"The difference of {num3} and {num4} is {difference}")
         except ValueError:
             print("Not a proper value")
-    elif option == ("Multiplication").strip().lower():
+    elif option == "3" or option == "multiply":
         try:
             num5 = int(input("Enter Num 5: "))
             num6 = int(input("Enter Num 6: "))
@@ -30,7 +29,7 @@ while check == False:
             print(f"The product of {num5} and {num6} is {product}")
         except ValueError:
             print("Not a proper value")
-    elif option == ("Divide").strip().lower():
+    elif option == "4" or option == "divide":
         try:
             num7 = int(input("Enter Num 7: "))
             num8 = int(input("Enter Num 8: "))
@@ -40,5 +39,5 @@ while check == False:
             print("Not a proper value")
         except ZeroDivisionError:
             print("Can't divide by 0")
-    elif option == ("Quit").strip().lower():
+    elif option == "quit":
         check == True
