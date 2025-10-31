7156068037dd7839c3cd57b8cc9e6c1ea5cfd805
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-10-31 14:42:00 -0400

    Update fileio.py

--- Notes/fileio.py
+++ Notes/fileio.py
@@ -1,6 +1,7 @@
 """
 Notes
 .docx are just .txts with mircrosoft overhead so when word parses it it styrips the overheads and properly displays it
+if both .txt file and .py file referencing the .txt are in the same folder use ls and cd commands to list and change directories to fix errors
 """
 
 msg = "hello \nworld"
@@ -8,9 +9,27 @@ print(msg)
 
 file = open("names.txt","r") #open("<filename>","<mode>")
 
-names = file.readline()
+buffer = file.readlines()
+names = []
+grades = []
+print(buffer)
+for line in buffer:
+    
+    line = line.strip()
+    line = line.split(",")
+    
+    
+    names.append(line[0])
+    grades.append(line[1])
+
+
+try:
+    
+    for i in range(len(grades)):
+        grades[i] = int(grades[i])
+except ValueError:
+    print("grade mustr be a number")
+
 print(names)
-for i in range(len(names)):
-        names[i] = names[i].strip()
-print(names)
+print(grades)
 file.close()
