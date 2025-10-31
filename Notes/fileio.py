6e49d08b018a84c65438eac24bbd70def59248f6
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-10-31 14:20:42 -0400

    Update fileio.py

--- Notes/fileio.py
+++ Notes/fileio.py
@@ -5,3 +5,12 @@ Notes
 
 msg = "hello \nworld"
 print(msg)
+
+file = open("names.txt","r") #open("<filename>","<mode>")
+
+names = file.readline()
+print(names)
+for i in range(len(names)):
+        names[i] = names[i].strip()
+print(names)
+file.close()
