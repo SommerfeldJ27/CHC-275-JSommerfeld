b43d27e2ce301ae88dd6f190c8435d9283ad378f
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-11-26 23:50:31 -0500

    Update new.py

--- Assignments/new.py
+++ Assignments/new.py
@@ -30,14 +30,9 @@ while check == False:
         quantity = float(input("How many would you like: "))
         try:
             index = items.index(item)
-            if item in cart:
-                index = cart.index(item)
-                cart_quantity[index] = quantity
-                cart_price[index] = quantity * prices[index]
-            else:
-                cart.append(item)
-                cart_quantity.append(quantity)
-                cart_price.append(quantity * prices[index])
+            cart.append(item)
+            cart_quantity.append(quantity)
+            cart_price.append(quantity * prices[index])
             print(f"Added {quantity} {item} to cart.")
         except ValueError:
             print("Item not found.")
@@ -55,8 +50,8 @@ while check == False:
                 cart_quantity.pop(index)
                 print(f"Removed {item} from cart.")
             else:
-                cart_quantity[index] = remove_quantity
-                cart_price[index] = remove_quantity * prices[index]
+                cart_quantity[index] = cart_quantity[index] - remove_quantity
+                cart_price[index] = cart_price[index] - remove_quantity * prices[index]
                 print(f"Removed {remove_quantity} {item} from cart.")
         except ValueError:
             print("Item not found.")
@@ -70,7 +65,7 @@ while check == False:
         tax = subtotal * statetax
         total = subtotal + tax
         print("Receipt:")
-        print(f"{cart} - {cart_price}")
+        print(f"{cart}")
         print(f"Subtotal: {subtotal}")
         print(f"Tax: {tax}")
         print(f"Total: {total}")
