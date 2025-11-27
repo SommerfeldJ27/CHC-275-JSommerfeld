b26528719992be7ee898afe4b4d142402f2f3d1a
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-11-26 23:51:29 -0500

    Update grocery.py

--- Assignments/grocery.py
+++ Assignments/grocery.py
@@ -4,6 +4,7 @@ items = []
 prices = []
 cart = []
 cart_price = []
+cart_quantity = []
 file.close()
 
 for line in buffer:
@@ -30,23 +31,33 @@ while check == False:
         try:
             index = items.index(item)
             cart.append(item)
+            cart_quantity.append(quantity)
             cart_price.append(quantity * prices[index])
             print(f"Added {quantity} {item} to cart.")
         except ValueError:
             print("Item not found.")
 
         print("Current cart:", cart)
+        
     elif option == "2":
         item = input("Enter item Name to remove: ").strip()
         try:
             index = cart.index(item)
-            cart.pop(index)
-            cart_price.pop(index)
-            print(f"Removed {item} from cart.")
+            remove_quantity = float(input("How many would you like to remove? "))
+            if remove_quantity >= cart_quantity[index]:
+                cart.pop(index)
+                cart_price.pop(index)
+                cart_quantity.pop(index)
+                print(f"Removed {item} from cart.")
+            else:
+                cart_quantity[index] = cart_quantity[index] - remove_quantity
+                cart_price[index] = cart_price[index] - remove_quantity * prices[index]
+                print(f"Removed {remove_quantity} {item} from cart.")
         except ValueError:
             print("Item not found.")
 
         print("Current cart:", cart)
+
     elif option == "3":
         check = True
         subtotal = sum(cart_price)
@@ -54,7 +65,7 @@ while check == False:
         tax = subtotal * statetax
         total = subtotal + tax
         print("Receipt:")
-        print(f"{cart} - {cart_price}")
+        print(f"{cart}")
         print(f"Subtotal: {subtotal}")
         print(f"Tax: {tax}")
         print(f"Total: {total}")
