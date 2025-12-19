5f88758d3bb6d29581a214e145a32cd6acbe6f80
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2025-12-19 11:21:20 -0500

    Update semproj.py

--- semproj.py
+++ semproj.py
@@ -18,7 +18,7 @@ losses = buffer[5].strip().split(":")
 losses.pop(0)
 
 for i in range(len(accounts)):
-    accounts[i] = accounts[i].strip()
+    accounts[i] = accounts[i].strip().lower()
     games_played[i] = int(games_played[i])
     kills[i] = int(kills[i])
     deaths[i] = int(deaths[i])
