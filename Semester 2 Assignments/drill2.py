b458edffac9e12956cd5c910f7d4cac93ba9a5e4
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2026-01-28 17:43:41 -0500

    Update drill2.py

--- Semester 2 Assignments/drill2.py
+++ Semester 2 Assignments/drill2.py
@@ -1,17 +1,16 @@
 def makeList():
     nums = []
-    while True:
-        user_input = input("Enter a number (or type 'stop'): ")
-        
-        if user_input.lower() == "stop":
-            break
+    check = False
+    while check == False:
+        option = input("Enter a number or type stop to finish: ").strip()
+
+        if option.strip().lower() == "stop":
+            check == True
+            return nums
         else:
-            nums.append(int(user_input))
-    
-    return nums
+            nums.append(int(option))
 
 list1 = makeList()
 print(list1)
-
 list2 = makeList()
 print(list2)
