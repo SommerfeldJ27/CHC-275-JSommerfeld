71539ac1df57b8a414a4396db0bcbbe95e558e30
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2026-02-20 13:14:40 -0500

    Update tictactoe.py

--- Semester 2 Assignments/tictactoe.py
+++ Semester 2 Assignments/tictactoe.py
@@ -4,49 +4,55 @@ board = [
     [0, 0, 0]
 ]
 
-def check_winner(p):
-    for i in range(3):
-        # rows
-        if board[i][0] == board[i][1] == board[i][2] == p:
-            return True
-        # columns
-        if board[0][i] == board[1][i] == board[2][i] == p:
-            return True
+for row in board:
+    for space in row:
+        print(space, end = " ")
+    print()
 
-    # diagonals
-    if board[0][0] == board[1][1] == board[2][2] == p:
-        return True
-    if board[0][2] == board[1][1] == board[2][0] == p:
-        return True
+row = int(input("Row (0-2): "))
+column = int(input("Column (0-2): "))
 
-    return False
-
-
-player = "X"
+player1 = "X"
+player2 = "O"
 moves = 0
 
-while moves < 9:
+if board[row][column] == player1 or board[row][column] == player2:
+    print("That space is already taken. Try again.")
+
+if moves < 9:
     # print board
     for row in board:
         print(row)
     print()
-
-    r = int(input("Row (0-2): "))
-    c = int(input("Column (0-2): "))
-
-    if board[r][c] == 0:
-        board[r][c] = player
-        moves += 1
-
-        if check_winner(player):
-            for row in board:
-                print(row)
-            print("Player", player, "wins!")
-            break
-
-        player = "O" if player == "X" else "X"
-    else:
-        print("Taken!")
-
-if moves == 9:
+elif moves == 9:
     print("Tie!")
+
+def winner(player1):
+    for i in range(len(board[0])):
+#Collum Win
+        if board[i][0] == board[i][1] == board[i][2] == player1:
+            return
+#Row Win
+        if board[0][i] == board[1][i] == board[2][i] == player1:
+            return
+#Diagonal Win       
+        if board[0][0] == board[1][1] == board[2][2] == player1:
+            return
+        if board[0][2] == board[1][1] == board[2][0] == player1:
+            return
+    print(f"Player {player1} wins")
+    
+def winner(player2):
+    for i in range(len(board[0])):
+#Collum Win
+        if board[i][0] == board[i][1] == board[i][2] == player2:
+            return
+#Row Win
+        if board[0][i] == board[1][i] == board[2][i] == player2:
+            return
+#Diagonal Win       
+        if board[0][0] == board[1][1] == board[2][2] == player2:
+            return
+        if board[0][2] == board[1][1] == board[2][0] == player2:
+            return
+    print(f"Player {player2} wins")
