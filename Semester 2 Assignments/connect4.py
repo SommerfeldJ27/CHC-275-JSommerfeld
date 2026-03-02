ccf43508703c8b3fcb0b664a099b0a265d0658c2
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2026-03-02 13:11:10 -0500

    Update connect4.py

--- Semester 2 Assignments/connect4.py
+++ Semester 2 Assignments/connect4.py
@@ -1,71 +1,89 @@
+"""
+Name: Jackson Sommerfeld
+Section: 275-2
+Description: Template for Lab 6
+"""
 
-current_player = "O"
+"""
+Scenario: We are to build a connect 4 game that runs in the terminal
+"""
+
+player = "O"
 board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
 ]
 
-def printBoard(board):
+def drawBoard(board):
     for row in board: 
         for space in row: 
             print(space, end="")
         print()
 
-def placePiece(row,col,board,current_player):
-
-    if board[row][col] == 0:
-        board[row][col]=current_player
-        return True
-    else: 
-        return False 
-    
-print()
-printBoard(board)
-placePiece(1,1,board,"O")
-print()
-printBoard(board)
-placePiece(1,1,board,"X")
-print()
-printBoard(board)
-
-def switchPlayer(current_player):
-    if current_player == "O":
+def switchPlayer(player):
+    if player == "O":
          return "X"
-    elif current_player == "X":
+    elif player == "X":
          return "O"
-        
 
-def checkWinner(board,current_player):
-    #Row Victories 
+def dropPiece(board,player,column):
+    for row in range(len(board)-1, -1, -1):
+        if board[row][column] == 0:
+            board[row][column] = player
+            return True
+    return False
+    
+def checkWinner(board,player):
+
+    # Row Victories 
     for i in range(len(board)):
-        if board[i][0] == board[i][1] == board[i][2] == current_player:
-           print(f"{current_player} wins")
-           return True
+        for j in range(len(board[0]) - 3):
+            if board[i][j] == board[i][j+1] == board[i][j+2] == board[i][j+3] == player:
+                print(f"{player} wins")
+                return True
     
-    #Column Victories
-    for i in range(len(board[0])):
-        if board[0][i] == board[1][i] == board[2][i] == current_player:
-            print(f"{current_player} wins")
-            return True
+    # Column Victories
+    for i in range(len(board) - 3):
+        for j in range(len(board[0])):
+            if board[i][j] == board[i+1][j] == board[i+2][j] == board[i+3][j] == player:
+                print(f"{player} wins")
+                return True
         
-    #Left Diagonal Victories
-    if board[0][0] == board[1][1] == board[2][2] == current_player:
-            print(f"{current_player} wins")
-            return True
+    # Left Diagonal Victories
+    for i in range(len(board) - 3):
+        for j in range(len(board[0]) - 3):
+            if board[i][j] == board[i+1][j+1] == board[i+2][j+2] == board[i+3][j+3] == player:
+                print(f"{player} wins")
+                return True
         
-    #Right diagonal victories
-    if board[0][2] == board[1][1] == board[2][0] == current_player:
-        print(f"{current_player} wins")
-        return True
-    
-    #Check for tie
-    for row in board:
-        for space in row:
-            if space == 0:
-                return False
+    # Right diagonal victories
+    for i in range(len(board) - 3):
+        for j in range(3, len(board[0])):
+            if board[i][j] == board[i+1][j-1] == board[i+2][j-2] == board[i+3][j-3] == player:
+                print(f"{player} wins")
+                return True
 
-    return True
+    return False
+    
+def main():
+    player = "O"
+    check = False
+
+    while check == False:
+        drawBoard(board)
+        column = int(input("Choose a column (0-6): "))
+        if dropPiece(board, player, column) == True:
+            if checkWinner(board, player) == True:
+                drawBoard(board)
+                check = True
+            else:
+                player = switchPlayer(player)
+        else:
+            print("Column is full. Try again.")
+
+if __name__ == "__main__":
+    main()
