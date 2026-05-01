f7be1045736284c3ae0e896ffdff0199d81a55f0
SommerfeldJ27 <sommerfeldj27@chcstudent.com>
2026-05-01 13:48:42 -0400

    Update lab8.py

--- Semester 2 Assignments/lab8.py
+++ Semester 2 Assignments/lab8.py
@@ -20,33 +20,26 @@ def switchPlayer(current_player):
     return current_player
 
 
-def find_inside(board):
-    territories = []
-    board_copy = [row[:] for row in board]
-    for i in range(len(board_copy)):
-        for j in range(len(board_copy[i])):
-            if board_copy[i][j] == '.':
-                region_cells = []
-                floodfill(board_copy, i, j, 't', region_cells)
-
-                area_size = 0
-                border_colors = set()
-                for x, y in region_cells:
-                    area_size += 1
-                    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                        nx, ny = x + dx, y + dy
-                        if 0 <= nx < len(board_copy) and 0 <= ny < len(board_copy[nx]):
-                            if board_copy[nx][ny] in ['w', 'b', 'W', 'B']:
-                                border_colors.add(board_copy[nx][ny])
-                        else:
-                            border_colors.add('edge')
-
-                if len(border_colors) == 1 and 'edge' not in border_colors:
-                    territories.append((region_cells, area_size, border_colors.pop()))
-
-                for x, y in region_cells:
-                    board_copy[x][y] = 'v'
-    return territories
+def find_inside(board,x,y,player):
+    #Base Case: bottom right
+    if x == len(board)-1 and y == len(board[0])-1:
+        return True
+    try:
+        if board[x][y] == "B":
+            player = "B"
+        if board[x][y] == "W":
+            player = "W"
+        if board[x][y] == board[x][y+1] == board [x + 1][y] == player and board[x+1][y+1] == '.':
+            floodfill(board,x+1,y+1,player)
+            return True
+        if board[x][y] == board [x][y + 1] == board[x+1][y-1] == player and board[x+1][y] == '.':
+            floodfill(board,x+1,y,player)
+            return True
+    finally:
+        if y < len(board[0])-2:    
+            find_inside(board,x,y+1,player)
+        if x < len(board)-2:
+            find_inside(board,x+1,y,player)
         
 
 def floodfill(matrix, x, y, color):
@@ -63,45 +56,42 @@ def floodfill(matrix, x, y, color):
             floodfill(matrix,x,y+1,color)
 
 
-def countScore(board, black_score, white_score):
+def countScore(board):
     black_score = 0
     white_score = 0
     
-    # Count stones
-    for row in board:
-        for cell in row:
-            if cell == "B":
-                black_score += 1
-            elif cell == "W":
-                white_score += 1
+    for i in range(len(board)):
+        for j in range(len(board[0])): 
+            if board[i][j] == "B":
+                black_score = black_score + 1
+            if board[i][j] == "W":
+                white_score = white_score + 1
     
     return black_score, white_score
 
+
 def printBoard(board):
-    for row in board:
-        line = ""
-        for cell in row:
-            line += cell + " "
-        print(line)
+    for row in board: 
+        for space in row: 
+            print(space, end=" ")
+        print()
 
 
-# WORK
 def main():
-    curr_player = 'w'
-    ques1 = input("Would you like to score an exisiting file? (y/n): ").strip().lower()
-    if ques1 == 'y':
-        input1 = input("What file would you like to load? (only the name not the .txt): ").strip()
-        board = loadBoard(input1)
-        printBoard(board)
-        w_score, b_score = countScore(board)
-        print(f"White Score: {w_score}")
-        print(f"Black score: {b_score}")
-        if w_score > b_score:
-            print("White wins!")
-        elif b_score > w_score:
-            print("Black wins!")
-        elif w_score == b_score:
-            print("It's a tie!")
-    
+    board = loadBoard("go_board.txt")
+    printBoard(board)
+    find_inside(board,0,0,"B")
+    print()
+    printBoard(board)
+    scores = countScore(board)
+
+    if scores[0] > scores[1]:
+        print("Black Wins")
+    if scores[0] < scores[1]:
+        print ("White Wins")
+    if scores[0] == scores[1]:
+        print("Its a Tie")
+
+
 if __name__ == "__main__":
     main()
